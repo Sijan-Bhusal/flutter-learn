403b1ce7b87ba87009159d3246381e81432e323c
Sijan <sijan.bhusal03@gmail.com>
2025-12-23 21:51:50 +0545

    fix: main.dart changed file

--- Dart-Basics/main.dart
+++ Dart-Basics/main.dart
@@ -1,3 +1,8 @@
-void main(){
-  print("Hello Sijan");
+void main() {
+  print(
+    (4 + 4) ~/ 3,
+  ); //~/ is integer (truncating) division in Dart. 8 ~/ 3 → 2 (int), prints 2.
+  print(
+    (4 + 4) / ~3,
+  ); // here ~3 represents the bitwise NOT operator equivalent to -4. SO it prints -2.0
 }
