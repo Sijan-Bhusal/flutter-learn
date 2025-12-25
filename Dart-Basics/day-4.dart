cab9adf9662aeeece88e7ab6165f6be550947b75
Sijan-Bhusal <sijan.bhusal03@gmail.com>
2025-12-25 18:54:43 +0545

    day-4 completed

---
+++ Dart-Basics/day-4.dart
@@ -0,0 +1,13 @@
+// Input = 3,5,2,1
+//Target code = [0,1]
+import 'dart:io';     // preprocessor directives
+void twoSum(){
+  print("Enter the numbers:");
+  int x,y;
+  x = int.parse(stdin.readLineSync()!);
+  print(x);
+}
+
+void main(){
+  twoSum();
+}
