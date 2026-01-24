eaf01bd5717849b484c54ee2bdd9b17a4f6c5063
sijan <sijan.bhusal03@gmail.com>
2026-01-24 16:25:25 +0545

    if-else problem

--- Dart-Basics/if-else-problem.dart
+++ Dart-Basics/if-else-problem.dart
@@ -6,13 +6,29 @@ void main() {
   String? destinationZone = stdin.readLineSync();
 
   //Conditions
-  if (destinationZone == 'PQR') {
-    print('Shipping cost is: ${weightinKG * 10}');
-  } else if (destinationZone == 'ABC') {
-    print('Shipping cost is: ${weightinKG * 5}');
-  } else if (destinationZone == 'XYZ') {
-    print('Shipping cost is: ${weightinKG * 9}');
-  } else {
-    print("Error!!");
+  // if (destinationZone == 'PQR') {
+  //   print('Shipping cost is: ${weightinKG * 10}');
+  // } else if (destinationZone == 'ABC') {
+  //   print('Shipping cost is: ${weightinKG * 5}');
+  // } else if (destinationZone == 'XYZ') {
+  //   print('Shipping cost is: ${weightinKG * 9}');
+  // } else {
+  //   print("Error!!");
+  // return;
+  // }
+
+  //Switch Cases
+  switch (destinationZone) {
+    case 'PQR':
+      print('Shipping cost is: ${weightinKG * 10}');
+      break;
+    case 'ABC':
+      print('Shipping cost is: ${weightinKG * 5}');
+      break;
+    case 'XYZ':
+      print('Shipping cost is: ${weightinKG * 20}');
+      break;
+    default:
+      print('Error!!');
   }
 }
