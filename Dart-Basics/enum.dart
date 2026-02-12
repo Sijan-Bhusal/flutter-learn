a4a1e49de0d05298c49efe3c9faf0b1347f3f7fa
Sijan-Bhusal <sijan.bhusal03@gmail.com>
2026-02-12 15:13:56 +0545

    feat: add enum dart

--- Dart-Basics/enum.dart
+++ Dart-Basics/enum.dart
@@ -3,10 +3,18 @@ void main() {
   final employee1 = Employee('Sijan', EmployeeType.swe);
   final employee2 = Employee('Rajan', EmployeeType.finance);
   employee2.fn();
-  print(employee1.type;
+  print(employee1.type);
+  print(employee1.type.runtimeType);
 }
 
-enum EmployeeType { swe, finance, marketing }
+enum EmployeeType {
+  swe(200000),
+  finance(300000),
+  marketing(400000);
+
+  final int Salary;
+  const EmployeeType(this.Salary);
+}
 
 class Employee {
   final String name;
@@ -15,6 +23,6 @@ class Employee {
   Employee(this.name, this.type);
 
   void fn() {
-    print(type.name);
+    print('${type.name} salary is ${type.Salary}');
   }
 }
