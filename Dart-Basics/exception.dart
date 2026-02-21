af75314f6575960747d64140fab2726c2f96aed5
Sijan-Bhusal <sijan.bhusal03@gmail.com>
2026-02-21 19:24:01 +0545

    add exception dart file

--- Dart-Basics/exception.dart
+++ Dart-Basics/exception.dart
@@ -1,8 +1,29 @@
+// Exception Handling
+// void main() {
+//   // Exception Example
+//   print(10 ~/ 3);
+//   // print(10 ~/ 0);
+//   try {
+//     print(10 ~/ 0);
+//   } on Exception catch (e) {
+//     print(e);
+//   } catch (e) {
+//     print('an error occured');
+//   }
+//   print('Sijan');
+// }
+
+// Future(promises)
+// future is good, as if you want to fetch external apis and it doesnt fetches data. IT uses exception handling extensively. It is aynchronous programming.
 void main() {
-  // Exception Example
-  print(10 ~/ 3);
-  print(10 / 10);
-  print(10 ~/ 0);
-print('Hello World')
-  print('Hello World');
+  print('Hello');
+  print('Hey');
+  print('Greetings'); //they are printed synchronously.
+  print(usePromise());
+}
+
+Future<String> usePromise() {
+  return Future(() {
+    return ('Hello');
+  });
 }
